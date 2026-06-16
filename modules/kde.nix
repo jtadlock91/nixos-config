54f98e99f3bd30e4fef6d6229f742562759763b1
jtadlock91 <jtadlock91@gmail.com>
2026-06-16 09:10:26 -0700

    feat: full KDE debloat matching Arch script

--- modules/kde.nix
+++ modules/kde.nix
@@ -1,29 +1,117 @@
 { config, pkgs, ... }:
 {
   services.xserver.enable = true;
-
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
   };
-
   services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    elisa
+    # PIM / Akonadi stack
+    akregator
+    kmail
+    korganizer
+    kontact
+    kaddressbook
+    knotes
+    kdepim-addons
     kdepim-runtime
+    akonadi
+    akonadi-calendar
+    akonadi-contacts
+    akonadi-mime
+    akonadi-search
+    kwalletmanager
+
+    # Utilities
+    ark
+    gwenview
+    okular
+    kate
+    kwrite
+    kcalc
+    kcharselect
+    kfind
+    kruler
+    kcolorchooser
+    kolourpaint
+    kgpg
+    ktimer
+    kmouth
+    kmag
+    kontrast
+    khelpcenter
+    konsole
+
+    # Multimedia
+    dragon
+    juk
+    kamoso
+    elisa
+    kwave
+
+    # Network apps
+    konqueror
+    kget
+    krdc
+    krfb
+    kdeconnect
+
+    # Games
+    bomber
+    bovo
+    granatier
+    kapman
+    katomic
+    kblackbox
+    kblocks
+    kbounce
+    kbreakout
+    kdiamond
+    kfourinline
+    kgoldrunner
+    kigo
+    killbots
+    kjumpingcube
+    klickety
+    klines
     kmahjongg
     kmines
-    konversation
+    knavalbattle
+    knetwalk
+    knights
+    kolf
+    kollision
+    konquest
     kpat
+    kreversi
+    ksirk
+    ksnakeduel
+    ksquares
     ksudoku
-    ktorrent
+    ktuberling
+    kubrick
+    lskat
+    palapeli
+    picmi
+
+    # Misc
     plasma-browser-integration
+    plasma-systemmonitor
+    kgamma
+    oxygen
+    baloo
+    print-manager
+    skanlite
   ];
 
   environment.variables = {
     NIXOS_OZONE_WL = "1";
   };
+
+  # Disable Baloo file indexer
+  services.baloo.enable = false;
 
   environment.systemPackages = with pkgs; [
     vivaldi

197a06c277f7084776177cdb6bc3583f366602bf
jtadlock91 <jtadlock91@gmail.com>
2026-06-16 16:08:12 -0700

    add: EasyEffects

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -8,4 +8,10 @@
   };
 
   hardware.graphics.enable32Bit = true;
+
+  # EasyEffects for audio DSP
+  programs.dconf.enable = true;
+  environment.systemPackages = with pkgs; [
+    easyeffects
+  ];
 }
